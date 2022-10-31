f6fab45043d49965a67123f5b244371ee21188ba
Abdul Ahad <ahadweb3.0@gmail.com>
2022-10-31 22:15:41 +0530

    deleted files

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-
-function components() {
-  return <h1>Nav Component</h1>;
-}
-
-ReactDOM.render(<components />, document.getElementById("root"));
